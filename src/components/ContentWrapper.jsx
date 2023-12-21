f8f480e1c068efce07ba75098149b6da94fe792f
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-12-21 21:16:23 +0600

    project complete

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -4,45 +4,64 @@ import TaskCard from "./TaskCard";
 import axiosInstance from "../utils/axios";
 import { UserContext } from "../App";
 
-const ContentWrapper = ({route}) => {
+const ContentWrapper = ({ route }) => {
+    const {
+        user: { accessToken },
+        tasks,
+        setTasks,
+    } = useContext(UserContext);
 
-  const {user:{accessToken},tasks,setTasks } =
-        useContext(UserContext);
-
-
-
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   
-  
+  let serverRoute;
 
-  const fetchTasks = async () => {
-    
-    const { data } = await axiosInstance.get('/get-task', {
-      headers: {
-        Authorization:`Bearer ${accessToken}`
-      }
-    })
-
-    setLoading(false)
-    setTasks(data)
-
+  if (route == 'complete') {
+    serverRoute = 'true'
+  } else if (route == 'incomplete') {
+    serverRoute = 'false'
+  } else {
+    serverRoute = 'AllTasks'
   }
 
-  useEffect(() => {
-    fetchTasks()
-  },[])
+    const fetchTasks = async () => {
+        const { data } = await axiosInstance.post(
+          "/get-task", {
+              completed: serverRoute,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
 
+        setLoading(false);
+        setTasks(data);
+    };
 
+    useEffect(() => {
+        fetchTasks();
+    }, [serverRoute]);
 
-  return (
-      <div className="h-full">
-          <h1 className="text-xl font-medium sm:text-2xl">All {tasks.length} Tasks</h1>
+    return (
+        <div className="h-full">
+            <h1 className="text-xl font-medium sm:text-2xl">
+                All {tasks.length} Tasks
+            </h1>
 
-          <hr className="border-white/20 mt-3 mb-10" />
+            <hr className="border-white/20 mt-3 mb-10" />
 
-          {tasks.length?loading ? <p>Loading...</p> : tasks.map((task, i) => <TaskCard key={i} task={task} />):"You Have no tasks"}
-      </div>
-  );
-}
+            {tasks.length ? (
+                loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    tasks.map((task, i) => <TaskCard key={i} task={task} />)
+                )
+            ) : (
+                "You Have no tasks"
+            )}
+        </div>
+    );
+};
 
-export default ContentWrapper
+export default ContentWrapper;
