5fb11f2bc2f771e9fb44d78c99d5627ba223d2d4
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-12-21 21:51:12 +0600

    change the server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: "https://todo-app-server-wv5w.onrender.com/",
     headers: {
-        'Content-Type':'application/json'
-    }
+        "Content-Type": "application/json",
+    },
 });
 
 export default axiosInstance
